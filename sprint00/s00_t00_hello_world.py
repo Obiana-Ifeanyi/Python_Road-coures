17b3a8eb7fc269e1262739c92c82106ce95ccc9d
gris <ifeanyiobiana@gmail.com>
2022-07-12 23:59:18 +0100

    hello_world!

---
+++ sprint00/s00_t00_hello_world.py
@@ -0,0 +1,7 @@
+## Create a script that prints the message below to the console:
+# 'Hello world! It is I, Arthur, son of Uther Pendragon, from the castle of Camelot.' 
+
+hello_world = 'Hello world! It is I, Arthur, son of Uther Pendragon, from the castle of Camelot.' 
+
+# To print text in Python, Use the print() fn.
+print (hello_world)
