9828b09054b7ccb63721ff9eacaa05c5eea74aa7
gris <ifeanyiobiana@gmail.com>
2022-07-13 00:16:26 +0100

    data types

---
+++ sprint00/s00_t01_data_types.py
@@ -0,0 +1,39 @@
+## What are data types?
+
+    # Data types are the classification or categorization of data items.
+    # It represents the kind of value that tells what operations can be performed on a particular data,
+    # since everything is an object in Python programming.
+
+## What data types are there in Python?
+
+    # Following are the standard or built-in data type of Python:
+        # Numeric
+            # integer
+            # float
+            # complex numbers
+        # Sequence Type
+            # strings
+            # tuple
+            # list
+        # Boolean
+        # Set
+        # Dictionary
+
+
+## Create a script that prints values in different data types:
+
+    # • integer with a value: 50
+    # • ﬂoat with a value: -42.00001
+    # • string with a value: 'Gris Money'
+    # • boolean with a value: True
+
+integer_value = 50
+float_value = -42.00001
+string_value = 'Gris Money'
+boolean_value = False
+
+
+print (integer_value)
+print (float_value)
+print (string_value)
+print (boolean_value)
