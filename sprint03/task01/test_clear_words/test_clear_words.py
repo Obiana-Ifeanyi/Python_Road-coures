6d4d26edbd1fbfa72256dd61b5cf661c5912a443
Gris <ifeanyiobiana@gmail.com>
2022-09-09 11:28:58 +0100

    test_clear_words

---
+++ sprint03/task01/test_clear_words/test_clear_words.py
@@ -0,0 +1,22 @@
+def clear_words(text):
+    import re
+
+    # var that stores our regex pattern
+    text_regex = re.compile(r'[?!.:;,-]+')
+
+    # check if pattern is text
+    if re.search(text_regex, text):
+
+        # split string per whitespace
+        list1 = text.split(" ")
+
+        # uses the function map() with a lambda expression to remove all punctuation marks in the split words
+        result = list(map(lambda list1: re.sub(text_regex, '', list1), list1))
+
+        return result
+
+
+
+text = 'WOMAN: Yes?, ENCYCLOPEDIA SALESMAN: Burglar, madam. WOMAN:' '\'Are you an encyclopaedia salesman?'
+
+print (clear_words(text))
